797583badea5f0497eff607c31c062308ac93e80
Carl <vezinaca@gmail.com>
2019-12-16 18:19:39 -0500

    Fix #1: Moved comments to TODO.md

--- scrape_catalogue.py
+++ scrape_catalogue.py
@@ -1,11 +1,4 @@
 #!/usr/bin/env python 3
-
-
-
-# DISCUSSION ENTRE GREG ET CARL
-# CARL - I am aware of the hard coded stuff in there.
-# GREG - Well, if you are aware of it, why are you keeping it in there?
-
 import sys
 import webbrowser
 import requests
@@ -52,23 +45,7 @@ def scrape_catalogue():
 		print(e.getText())
 		print ('\n')
 
-	#Would it be better if this function had a 'return'? i.e elementsTrouves = scrapeCatalogue()
-	#Then I would iterate and print the elementsTrouves array?
-
 get_html_file(url)
 scrape_catalogue()
 
 
-'''
-I'm able to get all the <a> tags that are in '#RMS_afficherIris .ValorisationListeDesc a'. 
-However as you can see, they are not all titles.  One of them in this case is a 
-link to 'Acces par PRETNUMERIQUE.CA (format ePub)', which I don't want.  I guess it doesn't 
-really matter, seeing as obviously this will never be a match with what I'm looking for.  I still
-think it's better to get rid of it. 
-
-To do so, I think I would need to 'extract' the <span class="pRchrContent"> from the document then go get
-the <a> tags the same way I'm proceeding now.
-
-I've tried a couple of things I saw in the documentation, but It doesn't seem to work.  
-
-'''
