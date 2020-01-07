5dfa289768df092488f75e01db3b9bbebb7b9f33
Carl <vezinaca@gmail.com>
2020-01-07 17:31:46 -0500

    Inserting borrowing history in local db

--- historique_emprunts_BANQ.py
+++ historique_emprunts_BANQ.py
@@ -5,7 +5,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from datetime import datetime
 import time
+
+import mysql.connector
 
 url = 'http://banq.qc.ca/mon_dossier/mon_dossier.html'
 browser = webdriver.Firefox(executable_path="/home/alienmint/Documents/Programmation/pythonPDF/gecko/geckodriver")
@@ -68,6 +71,10 @@ def printList(my_list):
 		#print("=====")
 
 if __name__ == "__main__":
+
+	cnx = mysql.connector.connect(user='root', password='root', host='127.0.0.1', database='banq')
+	mycursor = cnx.cursor()
+
 	connect_to_site(browser, url)
 	is_html_element_present_click(browser, 'Consulter mon dossier' , By.LINK_TEXT)
 	time.sleep(8)
@@ -99,89 +106,63 @@ if __name__ == "__main__":
 		#book_title = card.find_element_by_xpath("/div[2]/div[@class='cardContent_p5m42o']/div[1]/div[1]/div[1]/span[1]/div[1]/div[1]/span[1]")
 		#the_cards = card.find_elements_by_xpath("div[@class='cardMediaNoActions_xe8xza-o_O-color_1bra37d']")
 		
-		titles = card.find_elements_by_xpath("div[@class='cardStacked_n7d4vb']/div[@class='cardContent_p5m42o']/div[1]/div[1]/div[1]/span[1]/div[1]/div[1]/span[1]")
+		title = card.find_element_by_xpath("div[@class='cardStacked_n7d4vb']/div[@class='cardContent_p5m42o']/div[1]/div[1]/div[1]/span[1]/div[1]/div[1]/span[1]")
 		#label = card.find_elements_by_xpath("div[@class='cardStacked_n7d4vb']/div[@class='cardContent_p5m42o']/div[1]/div[1]/div[@class='metaFields_1su17lh']/div[1]/div[@class='meta-label metaLabel_13uwct0']")
 		meta_fields = card.find_elements_by_xpath("div[@class='cardStacked_n7d4vb']/div[@class='cardContent_p5m42o']/div[1]/div[1]/div[@class='metaFields_1su17lh']/div")
 		
 		borrowing_date = card.find_element_by_xpath("div[@class='cardStacked_n7d4vb']/div[@class='cardContent_p5m42o']/div[2]/div[@class='listContent_1nnce6d']/div[@class='cardContent_p5m42o']/div[1]/div[1]/div[1]/div[1]")
 		due_date = card.find_element_by_xpath("div[@class='cardStacked_n7d4vb']/div[@class='cardContent_p5m42o']/div[2]/div[@class='listContent_1nnce6d']/div[@class='cardContent_p5m42o']/div[1]/div[2]/div[1]/div[1]")
+		
+		borrowing_date_object = datetime.strptime(borrowing_date.text, '%m/%d/%Y')
+		due_date_object = datetime.strptime(due_date.text, '%m/%d/%Y')
+		
+		formatted_borrowing_date = borrowing_date_object.strftime('%Y-%m-%d %H:%M:%S')
+		formatted_due_date = due_date_object.strftime('%Y-%m-%d %H:%M:%S')
+
+
+		#author exists
 		if(len(meta_fields)>1):
 			author = meta_fields[0]
 			author_name = author.find_element_by_xpath("div[@class='meta-values metaValue_tcono5']/span[1]")
 			type_document = meta_fields[1]
 			type_document_name = type_document.find_element_by_xpath("div[@class='meta-values metaValue_tcono5']/span[1]")
 			print(author_name.text)
+		
+			#insert into db
+			sql = "INSERT INTO livre (title, author, type_document, borrowing_date, due_date) VALUES (%s, %s, %s, %s, %s)"
+			#val = ["The Great Gatsby", "Fitzgerald", "Printed Books", "2020-01-01", "2020-01-21"]
+			val = [str(title.text), str(author.text) , str(type_document.text), formatted_borrowing_date, formatted_due_date]
+			mycursor.execute(sql, val)
+
+			cnx.commit()
+
+			print(mycursor.rowcount, "record inserted.")
+			
+
+
+		#author doesn't exist
 		else:
 			#type_document = meta_fields.find_elements_by_xpath("div[1]/div[@class='meta-values metaValue_tcono5']")
 			type_document = meta_fields[0]
 			type_document_name = type_document.find_element_by_xpath("div[@class='meta-values metaValue_tcono5']/span[1]")
 			
+			#insert into db
+			
+			sql = "INSERT INTO livre (title, author, type_document, borrowing_date, due_date) VALUES (%s, %s, %s, %s, %s)"
+			val = [str(title.text), "" , str(type_document.text), formatted_borrowing_date, formatted_due_date]
+			mycursor.execute(sql, val)
 
+			cnx.commit()
+
+			print(mycursor.rowcount, "record inserted.")
+			
+		print(title.text)
 		print(type_document_name.text)
 		print(borrowing_date.text)
 		print(due_date.text)
 
-		label = card.find_elements_by_xpath("div[@class='cardStacked_n7d4vb']/div[@class='cardContent_p5m42o']/div[1]/div[1]/div[@class='metaFields_1su17lh']/div[1]/div[@class='meta-label metaLabel_13uwct0']")
-		value = card.find_elements_by_xpath("div[@class='cardStacked_n7d4vb']/div[@class='cardContent_p5m42o']/div[1]/div[1]/div[@class='metaFields_1su17lh']/div[1]/div[@class='meta-values metaValue_tcono5']/span[1]")
+		#label = card.find_elements_by_xpath("div[@class='cardStacked_n7d4vb']/div[@class='cardContent_p5m42o']/div[1]/div[1]/div[@class='metaFields_1su17lh']/div[1]/div[@class='meta-label metaLabel_13uwct0']")
+		#value = card.find_elements_by_xpath("div[@class='cardStacked_n7d4vb']/div[@class='cardContent_p5m42o']/div[1]/div[1]/div[@class='metaFields_1su17lh']/div[1]/div[@class='meta-values metaValue_tcono5']/span[1]")
 		
+	cnx.close()	
 		
-		#print(str(len(titles)))
-		#print(str(len(label)))
-		#print(str(len(value)))
-		#print(str(len(meta_fields)))
-
-		#printList(titles)
-		#printList(label)
-		#printList(value)
-	
-
-'''
-
-all_book_titles = browser.find_elements_by_xpath("//div[@class='cardContent_p5m42o']/div[1]/div[1]/div[1]/span[1]/div[1]/div[1]/span[1]") 
-#all_book_authors = browser.find_elements_by_xpath("//div[@class='meta-values metaValue_tcono5']/span[1]") 
-
-all_book_authors = browser.find_elements_by_xpath("//div[@class='metaFields_1su17lh']/div[1]/div[@class='meta-values metaValue_tcono5']/span[1]")
-#all_book_types = browser.find_elements_by_xpath("//div[@class='metaFields_1su17lh']/div[2]/div[@class='meta-values metaValue_tcono5']/span[1]") 
-
-all_borrowing_dates = browser.find_elements_by_xpath("//div[@class='listContent_1nnce6d']/div[@class='cardContent_p5m42o']/div[1]/div[1]/div[1]/div[1]")
-all_due_dates = browser.find_elements_by_xpath("//div[@class='listContent_1nnce6d']/div[@class='cardContent_p5m42o']/div[1]/div[2]/div[1]/div[1]")
-
-#<div class="listContent_1nnce6d" style="padding: 8px 0px;"><div class="cardContent_p5m42o">
-
-#both in metaFields_1su17lh
-#<div class="meta-label metaLabel_13uwct0"> type of document / author
-#<div class="meta-values metaValue_tcono5"><span>Printed Books</span></div> this can be name of author or printed books
-
-
-print ("size of all book titles: " + str(len(all_book_titles)))
-print ("size of all book authors: " + str(len(all_book_authors)))
-print ("size of all borrowing dates: " + str(len(all_borrowing_dates)))
-print ("size of all due dates: " + str(len(all_due_dates)))
-#print ("size of all book types: " + str(len(all_book_types)))
-fichier_historique = 'historique_emprunt.txt'
-
-open(fichier_historique, 'w').close()
-
-
-
-printList(all_book_titles)
-print ("=======================================")
-printList(all_book_authors)
-print ("=======================================")
-printList(all_borrowing_dates)
-print ("=======================================")
-printList(all_due_dates)
-
-#printList(all_book_types)
-'''
-
-'''
-file_to_write = open(fichier_historique, "a")
-i = 1
-for book in all_book_titles:
-	file_to_write.write(str(i) + ") " + book.text + '\n')
-	i = i + 1
-
-file_to_write.close()
-
-'''
