de2209a6c4d3610b92e4d4b555135be56cb6b4b9
Carl <vezinaca@gmail.com>
2020-01-07 14:47:54 -0500

    Fixed #25

--- renouvellement_BANQ.py
+++ renouvellement_BANQ.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from datetime import datetime
 import time
 
 url = 'http://banq.qc.ca/mon_dossier/mon_dossier.html'
@@ -68,32 +69,61 @@ def printList(my_list):
 		print("=====")
 
 if __name__ == "__main__":
+
+	today = datetime.now()
 	connect_to_site(browser, url)
 	is_html_element_present_click(browser, 'Consulter mon dossier' , By.LINK_TEXT)
 	time.sleep(8)
 	#is_html_element_present_click(browser, "//*[contains(text(), 'Borrowing history') or contains(text(), 'Historique des emprunts')]", By.XPATH)
 	is_html_element_present_click(browser, "//*[contains(text(), 'Borrowed and renewed items')]", By.XPATH)
 	
-	#infinite_scroll()
 	time.sleep(4)
+	infinite_scroll()
+	time.sleep(4)
+	
 	#//input[starts-with(@id, 'activation:') and contains(@id, ':voId:1')]
 	#all_cards = browser.find_elements_by_xpath("//div[starts-with(@class, 'cardContent_')]") 
-	#<div class="cardStacked_n7d4vb"><div class="cardContent_p5m42o">
-	#all_cards = browser.find_elements_by_xpath("//div[@class='cardContent_p5m42o']") 
-	#all_book_titles = browser.find_elements_by_xpath("//div[@class='cardContent_p5m42o']/div[1]/div[1]/div[1]/span[1]/div[1]/div[1]/span[1]") 
-
-	#all_cards_main = browser.find_elements_by_xpath("//div[@class='cardStacked_n7d4vb']")
-	#<div class="card_dzxwpk"><div class="cardMediaNoActions_xe8xza-o_O-color_1bra37d" title="Experiencing Nirvana : grunge in Europe, 1989">
-	#all_cards_stacked = browser.find_elements_by_xpath("//div[starts-with(@class,'cardStacked_')]")
+	
 	all_cards_stacked = browser.find_elements_by_xpath("//div[starts-with(@class,'card_dzxwpk')]")
 
 	print("size of all cards main: " + str(len(all_cards_stacked)))
 
+	all_renew_buttons_to_click = []
 	for card in all_cards_stacked:
-		#the_cards = card.find_elements_by_xpath("//div[starts-with(@class,'cardContent_p5m42o')]")
-		#<div class="cardMediaNoActions_xe8xza-o_O-color_1bra37d" title="Amsterdam - 2017">
-		#username = driver.find_element_by_xpath("//input[@name='username']")
-		book_title = card.find_element_by_xpath("//div[@class='cardContent_p5m42o']/div[1]/div[1]/div[1]/span[1]/div[1]/div[1]/span[1]")
-		print(book_title.text)
+		
+		title = card.find_element_by_xpath("div[@class='cardStacked_n7d4vb']/div[@class='cardContent_p5m42o']/div[1]/div[1]/div[1]/span[1]/div[1]/div[1]/span[1]")
+		borrowing_date = card.find_element_by_xpath("div[@class='cardStacked_n7d4vb']/div[@class='cardContent_p5m42o']/div[2]/div[@class='listContent_1nnce6d']/div[@class='cardContent_p5m42o']/div[1]/div[1]/div[1]/div[1]")
+		due_date = card.find_element_by_xpath("div[@class='cardStacked_n7d4vb']/div[@class='cardContent_p5m42o']/div[2]/div[@class='listContent_1nnce6d']/div[@class='cardContent_p5m42o']/div[1]/div[2]/div[1]/div[1]")
+		
+		#renew_button_text = card.find_element_by_xpath("div[@class='cardStacked_n7d4vb']/div[2]/div[@class='cardActions_1423utz']/button[1]/div[1]/span[1]")				
+		renew_button = card.find_element_by_xpath("div[@class='cardStacked_n7d4vb']/div[2]/div[@class='cardActions_1423utz']/button[1]")				
+		
+		print("====begin====")
+		print(renew_button.text)
+		print("====end====")
+
+
+		if(renew_button.text == "RENEW"):
+
+			due_date_object = datetime.strptime(due_date.text, '%m/%d/%Y')
+
+			difference_date = (due_date_object - today).days
+			print(str(difference_date))
+			print(difference_date < 16)
+			if(difference_date < 16):
+				all_renew_buttons_to_click.append(renew_button)
+				#renew_button.click()
+
+
+		print(title.text)
+		print(borrowing_date.text)
+		print(due_date.text)
+
+
+
+
+	for button in all_renew_buttons_to_click:
+		print("ca click!")
+		#button.click()
 
 
