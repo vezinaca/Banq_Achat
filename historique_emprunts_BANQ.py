3eb1cf094ba197cc76b8180a142f6be6015557dd
Carl <vezinaca@gmail.com>
2020-01-08 07:54:09 -0500

    cleaned up code in historique_emprunts_BANQ.py 2

--- historique_emprunts_BANQ.py
+++ historique_emprunts_BANQ.py
@@ -88,7 +88,7 @@ if __name__ == "__main__":
 
 	connect_to_site(browser, url)
 	is_html_element_present_click(browser, 'Consulter mon dossier' , By.LINK_TEXT)
-	time.sleep(8)
+	time.sleep(4)
 	is_html_element_present_click(browser, "//*[contains(text(), 'Borrowing history') or contains(text(), 'Historique des emprunts')]", By.XPATH)
 
 	#infinite_scroll()
