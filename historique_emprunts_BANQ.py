7f25ca4059e43390325640344e6490c07f684e64
Carl <vezinaca@gmail.com>
2020-01-08 07:48:53 -0500

    cleaned up code in historique_emprunts_BANQ.py

--- historique_emprunts_BANQ.py
+++ historique_emprunts_BANQ.py
@@ -73,44 +73,34 @@ def printList(my_list):
 def delete_all_table_rows(table):
 	pass
 
+def insert_into_db(cnx, cursor, valeurs):
+	
+	sql = "INSERT INTO livre (title, author, type_document, borrowing_date, due_date) VALUES (%s, %s, %s, %s, %s)"
+	mycursor.execute(sql, val)
+	cnx.commit()
+	print(mycursor.rowcount, "record inserted.")
+
 if __name__ == "__main__":
 
 	cnx = mysql.connector.connect(user='root', password='root', host='127.0.0.1', database='banq')
 	mycursor = cnx.cursor()
-	mycursor.execute("TRUNCATE TABLE livre")
+	#mycursor.execute("TRUNCATE TABLE livre")
 
 	connect_to_site(browser, url)
 	is_html_element_present_click(browser, 'Consulter mon dossier' , By.LINK_TEXT)
 	time.sleep(8)
 	is_html_element_present_click(browser, "//*[contains(text(), 'Borrowing history') or contains(text(), 'Historique des emprunts')]", By.XPATH)
-	#is_html_element_present_click(browser, "//*[contains(text(), 'Borrowed and renewed items')]", By.XPATH)
-	
-
 
 	#infinite_scroll()
 	time.sleep(4)
 	#//input[starts-with(@id, 'activation:') and contains(@id, ':voId:1')]
-	#all_cards = browser.find_elements_by_xpath("//div[starts-with(@class, 'cardContent_')]") 
-	#<div class="cardStacked_n7d4vb"><div class="cardContent_p5m42o">
-	#all_cards = browser.find_elements_by_xpath("//div[@class='cardContent_p5m42o']") 
 	all_book_titles = browser.find_elements_by_xpath("//div[@class='cardContent_p5m42o']/div[1]/div[1]/div[1]/span[1]/div[1]/div[1]/span[1]") 
-
-	#all_cards_main = browser.find_elements_by_xpath("//div[@class='cardStacked_n7d4vb']")
-	#<div class="card_dzxwpk"><div class="cardMediaNoActions_xe8xza-o_O-color_1bra37d" title="Experiencing Nirvana : grunge in Europe, 1989">
-	#all_cards_stacked = browser.find_elements_by_xpath("//div[starts-with(@class,'cardStacked_')]")
 	all_cards_stacked = browser.find_elements_by_xpath("//div[starts-with(@class,'card_dzxwpk')]")
 
 	print("size of all cards main: " + str(len(all_cards_stacked)))
 	print ("size of all book titles: " + str(len(all_book_titles)))
-
-	#printList(all_book_titles)
-	#printList(all_cards_stacked)
+	
 	for card in all_cards_stacked:
-		#the_cards = card.find_elements_by_xpath("//div[starts-with(@class,'cardContent_p5m42o')]")
-		#<div class="cardMediaNoActions_xe8xza-o_O-color_1bra37d" title="Amsterdam - 2017">
-		#username = driver.find_element_by_xpath("//input[@name='username']")
-		#book_title = card.find_element_by_xpath("/div[2]/div[@class='cardContent_p5m42o']/div[1]/div[1]/div[1]/span[1]/div[1]/div[1]/span[1]")
-		#the_cards = card.find_elements_by_xpath("div[@class='cardMediaNoActions_xe8xza-o_O-color_1bra37d']")
 		
 		title = card.find_element_by_xpath("div[@class='cardStacked_n7d4vb']/div[@class='cardContent_p5m42o']/div[1]/div[1]/div[1]/span[1]/div[1]/div[1]/span[1]")
 		#label = card.find_elements_by_xpath("div[@class='cardStacked_n7d4vb']/div[@class='cardContent_p5m42o']/div[1]/div[1]/div[@class='metaFields_1su17lh']/div[1]/div[@class='meta-label metaLabel_13uwct0']")
@@ -120,7 +110,9 @@ if __name__ == "__main__":
 		due_date = card.find_element_by_xpath("div[@class='cardStacked_n7d4vb']/div[@class='cardContent_p5m42o']/div[2]/div[@class='listContent_1nnce6d']/div[@class='cardContent_p5m42o']/div[1]/div[2]/div[1]/div[1]")
 		
 		borrowing_date_object = datetime.strptime(borrowing_date.text, '%m/%d/%Y')
+		#borrowing_date_object = datetime.strptime(borrowing_date.text, '%d/%m/%Y')
 		due_date_object = datetime.strptime(due_date.text, '%m/%d/%Y')
+		#due_date_object = datetime.strptime(due_date.text, '%d/%m/%Y')
 		
 		formatted_borrowing_date = borrowing_date_object.strftime('%Y-%m-%d %H:%M:%S')
 		formatted_due_date = due_date_object.strftime('%Y-%m-%d %H:%M:%S')
@@ -133,35 +125,15 @@ if __name__ == "__main__":
 			type_document = meta_fields[1]
 			type_document_name = type_document.find_element_by_xpath("div[@class='meta-values metaValue_tcono5']/span[1]")
 			print(author_name.text)
-		
-			#insert into db
-			sql = "INSERT INTO livre (title, author, type_document, borrowing_date, due_date) VALUES (%s, %s, %s, %s, %s)"
-			#val = ["The Great Gatsby", "Fitzgerald", "Printed Books", "2020-01-01", "2020-01-21"]
 			val = [str(title.text), str(author_name.text) , str(type_document_name.text), formatted_borrowing_date, formatted_due_date]
-			mycursor.execute(sql, val)
-
-			cnx.commit()
-
-			print(mycursor.rowcount, "record inserted.")
 			
-
-
 		#author doesn't exist
 		else:
-			#type_document = meta_fields.find_elements_by_xpath("div[1]/div[@class='meta-values metaValue_tcono5']")
 			type_document = meta_fields[0]
 			type_document_name = type_document.find_element_by_xpath("div[@class='meta-values metaValue_tcono5']/span[1]")
-			
-			#insert into db
-			
-			sql = "INSERT INTO livre (title, author, type_document, borrowing_date, due_date) VALUES (%s, %s, %s, %s, %s)"
 			val = [str(title.text), "" , str(type_document_name.text), formatted_borrowing_date, formatted_due_date]
-			mycursor.execute(sql, val)
-
-			cnx.commit()
-
-			print(mycursor.rowcount, "record inserted.")
-			
+		
+		insert_into_db(cnx, mycursor, val)	
 		print(title.text)
 		print(type_document_name.text)
 		print(borrowing_date.text)
