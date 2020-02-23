b8ab5b3cf0da0c5feaa622052b9d8116134c53f7
Carl <vezinaca@gmail.com>
2020-02-23 18:30:39 -0500

    tested with whole db

--- verify_orders_banq.py
+++ verify_orders_banq.py
@@ -12,7 +12,7 @@ from datetime import date
 
 def update_received_db(cnx, mycursor, valeurs):
 	
-	sql = "UPDATE orders_test SET received = %s WHERE isbn = %s"
+	sql = "UPDATE orders SET received = %s WHERE isbn = %s"
 	mycursor.execute(sql, valeurs)
 	cnx.commit()
 	print(mycursor.rowcount, "record updated.")
@@ -21,7 +21,7 @@ def update_received_db(cnx, mycursor, valeurs):
 
 def update_last_checked_db(cnx, mycursor, valeurs):
 	
-	sql = "UPDATE orders_test SET last_checked = %s WHERE isbn = %s"
+	sql = "UPDATE orders SET last_checked = %s WHERE isbn = %s"
 	mycursor.execute(sql, valeurs)
 	cnx.commit()
 	print(mycursor.rowcount, "record updated.")
@@ -44,7 +44,7 @@ if __name__ == '__main__':
 	#browser.quit()
 
 	email_string = 'Subject: New book availibility verification for ' + str(date.today()) + '\n'
-	email_string = email_string + "\nGreetings bookworm, \n\nHere are the verifications: \n\n"
+	email_string = email_string + "\nGreetings bookworm, \n\nHere are the verifications:"
 	
 	
 	headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1'}
@@ -57,7 +57,7 @@ if __name__ == '__main__':
 	creds = ('00115446', '19771314')
 	connect_to_site(browser, url, creds)
 
-	sql_select_all = """SELECT * FROM orders_test WHERE received=0"""
+	sql_select_all = """SELECT * FROM orders WHERE received=0"""
 	my_cursor.execute(sql_select_all)
 	record = my_cursor.fetchall()
 
@@ -78,43 +78,44 @@ if __name__ == '__main__':
 		#browser.get(formulaire_link)
 
 		if (len(my_books_banq) == 0):
-			print('The book ' + titre + ' by ' + auteur + + " ISBN13: " + isbn_du_livre_recherche +' was NOT found in the BANQ catalogue.')
-			email_string = email_string + '\n\nThe book ' + titre + ' by ' + auteur + " ISBN13: " + isbn_du_livre_recherche +  ' was NOT found in the BANQ catalogue.'
+			print('The book ' + titre + ' (' + isbn_du_livre_recherche + ') by ' + auteur + ' was NOT found in the BANQ catalogue.')
+			email_string = email_string + '\n\nThe book ' + titre + ' (' +  isbn_du_livre_recherche + ') by ' + auteur + ' was NOT found in the BANQ catalogue.'
 			#print('The book ' + book_search_scrape_isbn.list_of_dic_books[0].get('Titre') + ' by ' + book_search_scrape_isbn.list_of_dic_books[0].get('Auteur') + ' will be ordered.')
 		else:
 			print('The book ' + titre + ' by ' + auteur + ' was FOUND in the BANQ catalogue.')
-			email_string = email_string + '\n\nThe book ' + titre + ' by ' + auteur + " ISBN13: "+ isbn_du_livre_recherche + ' was FOUND in the BANQ catalogue.'
+			email_string = email_string + '\n\nThe book ' + titre + ' (' + isbn_du_livre_recherche + ') by ' + auteur + ' was FOUND in the BANQ catalogue.'
 			url_recherche_selenium = 'https://www.banq.qc.ca/techno/recherche/rms.html?q=' + isbn_du_livre_recherche
 			browser.get(url_recherche_selenium)
 			time.sleep(3)
 			book_link = browser.find_element_by_xpath("//div[starts-with(@class,'ValorisationListeDesc')]/a")
 			book_link.click()
 
-			time.sleep(3)
+			time.sleep(5)
 			#on loan
 			#book_status = browser.find_element_by_xpath("//div[starts-with(@class,'_290')]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]")
 			#Available
 			book_status = browser.find_element_by_xpath("//div[starts-with(@class,'_290')]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]")
-			print("this book status: " + book_status.text)
+			print("Book status: " + book_status.text)
 			#print("this book status avail: " + book_status_avail.text)
 			if ('Available' in book_status.text):
-				email_string = email_string + '\n\nThe book ' + titre + " by " + auteur + " ISBN13: " + isbn_du_livre_recherche + ' was FOUND and is AVAILABLE in the BANQ catalogue.'
-				print('The book ' + titre + " by " + auteur + " ISBN13: " +  isbn_du_livre_recherche + ' is available in the BANQ catalogue.')
+				#email_string = email_string + '\n\nThe book ' + titre + " by " + auteur + " ISBN13: " + isbn_du_livre_recherche + ' was FOUND and is AVAILABLE in the BANQ catalogue.'
+				email_string = email_string + "\n\t\t This book is currently available"
+				print('The book ' + titre + ' (' + isbn_du_livre_recherche +  ") by " + auteur + ' is available in the BANQ catalogue.')
 
 			# need to verify what is written on website when books says: 'en commande', reserve it anyways
 			#if ('loan' in book_status.text):
 			else:
-				email_string = email_string + "\n\t\t This book is on loan but will be reserved"
-				print("oui c emprunte")
+				email_string = email_string + "\n\t\t This book is " + book_status.text + " but will be RESERVED."
+				#print("oui c emprunte")
 				reserver_button = browser.find_element_by_xpath("//div[starts-with(@class,'_290')]/div[1]/div[1]/div[1]/div[1]/button[1]")
-				print("texte du boutton: " + reserver_button.text)
+				#print("texte du boutton: " + reserver_button.text)
 				reserver_button.click()
-				time.sleep(8)
+				time.sleep(7)
 				confirm_popup_button = browser.find_element_by_xpath("//body/div[4]/div[1]/div[1]/div[1]/div[1]/div[2]/button[1]")
 				confirm_popup_button.click()
-				time.sleep(4)
+				time.sleep(6)
 				close_popup_button = browser.find_element_by_xpath("//body/div[4]/div[1]/div[1]/div[1]/div[1]/div[2]/button[1]")
-				print("texte du close_popup_boutton: " + close_popup_button.text)
+				#print("texte du close_popup_boutton: " + close_popup_button.text)
 				close_popup_button.click()
 
 			#table name, field, value of field, isbn
